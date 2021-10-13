fe590b30dfaec01d119492060f7d2aa2c71eabed
Rui Pinho <rmapinho@gmail.com>
2021-10-13 15:43:11 +0100

    fix package.json

--- src/components/NavbarComp.js
+++ src/components/NavbarComp.js
@@ -5,7 +5,7 @@ import Home from "./Home";
 import About from "./About";
 import Contact from "./Contact";
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"; //Routing
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom"; //Routing
 
 export default class NavbarComp extends Component {
   render() {
